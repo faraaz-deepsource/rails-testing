4dd2773fcff77c9ee6efdc658c330ada38d38233
Syed Faraaz Ahmad <113512303+faraaz-deepsource@users.noreply.github.com>
2023-07-13 14:32:02 +0530

    Update person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,5 +2,6 @@ class Person < ApplicationRecord
   after_commit :after_commit_validation
   before_validation :before_validation_callback
 
+  scope :updated_in_last_7_days, -> { where('updated_at BETWEEN ? AND ?', 7.days.ago.beginning_of_day, Time.current) }
   enum status: [:active, :archived]
 end
