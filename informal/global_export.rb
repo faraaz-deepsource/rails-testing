71635f2bfbfd85cb8291e6ae48b61a70e43b14d7
Syed Faraaz Ahmad <113512303+faraaz-deepsource@users.noreply.github.com>
2023-08-08 13:33:04 +0530

    Update global_export.rb

--- informal/global_export.rb
+++ informal/global_export.rb
@@ -1,8 +1,8 @@
-module FinancialType
-  def self.find_by_name(name)
-    # do something
-  end
-end
+# module FinancialType
+#   def self.find_by_name(name)
+#     # do something
+#   end
+# end
 
 def pending_changes_financial_types(types)
   types.map { |ft| FinancialType.find_by_name(ft) }
