87b9f98870bcf24bab0b9c48a39b3545a8f888d2
Syed Faraaz Ahmad <113512303+faraaz-deepsource@users.noreply.github.com>
2023-08-21 10:25:03 +0530

    Update person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,5 @@ class Person < ApplicationRecord
   before_validation :before_validation_callback
 
   enum status: [:active, :archived]
+  attribute :confirmed_at, :datetime, default: Time.zone.now
 end
